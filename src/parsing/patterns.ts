fa1e39cbd5a6c9c6edae9b43649f12b5baf1bf6c
Ossama W. Obeid <owo@owobeid.com>
2025-12-23 18:07:48 +0400

    Fixed parsing issue when a direction marker appears in the content of a speech entry.

--- src/parsing/patterns.ts
+++ src/parsing/patterns.ts
@@ -11,7 +11,7 @@ const COMMENT_SINGLE = /#/.source;
 const COMMENT_FENCE = /(?<fence>###+)/.source;
 const CAPSULE_MARKER = /(?<capsule>\(\))?/.source;
 
-const SPEECH_PARAMS = /(?<speechParams>.*)/.source;
+const SPEECH_PARAMS = /(?<speechParams>.*?)/.source;
 const SPEECH_DIR_SINGLE = /(?<speechDir>[<>^])/.source;
 const SPEECH_DIR_FENCE = /(?<fence>(<<<+)|(>>>+)|(\^\^\^+))/.source;
 const SPEECH_HIDE_NAME_MARKER = /(?<hideName>!)?/.source;
